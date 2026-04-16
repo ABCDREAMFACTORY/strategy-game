759bf7563decf637c5e866b483511336db704726
Timothée <timotheeriviere10@gmail.com>
2026-04-16 22:32:40 +0200

    added list selector for selecting civ's players

--- scripts/Menus/GameSettingsScreen/GameSettings.py
+++ scripts/Menus/GameSettingsScreen/GameSettings.py
@@ -1,5 +1,5 @@
 from ..BaseMenu.Menu import Menu
-from ...graphics.hud import Button, IntSelector
+from ...graphics.hud import Button, IntSelector, ListSelector
 from ...core.Player import Player
 from ...core.Position import Position
 from ...core.gameManager import GameManager
@@ -7,10 +7,12 @@ from ...core.gameManager import GameManager
 class GameSettingsMenu(Menu):
     def __init__(self, game):
         self.game = game
+        self.players = [Player(self.game, name=f"player{iplayer}", civ_name="France", start_position=Position(3*iplayer,0)) for iplayer in range(2)]
         objects = [
-            IntSelector(id="nbplayers", x=500, y=300, width=200, height=50, text="Number of players", value=2, min_value=2, max_value=100),
+            IntSelector(id="nbplayers", x=500, y=300, width=200, height=50, text="Number of players", value=2, min_value=2, max_value=100, on_click=self.on_nb_players_change),
             Button(id=1,x=500, y=400, width=200, height=50, text="Start game", func=lambda:self.start_new_game()),
             Button(id=2,x=500, y=470, width=200, height=50, text="Back to main menu", func=lambda: self.change_menu("main")),
+            ListSelector(id="civselector", x=900, y=230, width=200, height=50, text="Joueurs", options=[player.name for player in self.players])
         ]
         super().__init__(game, "settings", objects)
 
@@ -19,8 +21,8 @@ class GameSettingsMenu(Menu):
         if nbplayers_selector is None:
             raise ValueError("Number of players selector not found!")
         players = []
-        for iplayer in range(nbplayers_selector.value):
-            players.append(Player(self.game, civ_name="France", start_position=Position(3*iplayer,0)))
+        for iplayer in range(nbplayers_selector.value): # type: ignore
+            players.append(Player(self.game, name=f"player{iplayer}", civ_name="France", start_position=Position(3*iplayer,0)))
             
 
         self.game.initialize_game_manager(GameManager(
@@ -30,3 +32,9 @@ class GameSettingsMenu(Menu):
             map_height=50
         ))
         self.change_menu("game")
+
+    def on_nb_players_change(self):
+        self.players = [Player(self.game, name=f"player{iplayer}", civ_name="France", start_position=Position(3*iplayer,0)) for iplayer in range(self.getObjectById("nbplayers").value)] # type: ignore
+        civ_selector = self.getObjectById("civselector")
+        if civ_selector is not None:
+            civ_selector.new_list([player.name for player in self.players]) # type: ignore
